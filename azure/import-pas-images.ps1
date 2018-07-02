1e0458e5e3692c5a5d311305eca558199abcfe31
Ziv Lifshits <Ziv.Lifshits@Cyberark.com>
2018-07-02 15:40:57 +0300

    Add import-pas-images script

---
+++ azure/import-pas-images.ps1
@@ -0,0 +1,61 @@
+PARAM(
+    # location to import Cyberark images to
+    [Parameter(Mandatory=$true)]
+    [String]
+    $location,
+    # Supplied by CyberArk components AccessSAS
+    [Parameter(Mandatory=$true)]
+    [String]
+    $ComponentsAccessSAS,
+    # Supplied by CyberArk PSMP AccessSAS
+    [Parameter(Mandatory=$true)]
+    [String]
+    $PsmpAccessSAS
+)
+ 
+#Set variables
+$storageName = "cyberarkimages"
+$containerName = "cyberarkimages"
+$componentsDestBlob = "pas-components.vhd"
+$psmpDestBlob="pas-psmp.vhd"
+$resourceGroupName = "Cyberark-Images"
+ 
+#Create Resource Group
+$resourceGroup = New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+ 
+#Create Storage Account
+$storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageName -Location $location -SkuName Standard_LRS
+$destContext = $storageAccount.Context
+ 
+#Create Blob Storage Container
+$container = New-AzureStorageContainer -Name $containerName -Permission Off -Context $destContext
+ 
+#Start copy components
+$componentsblobcopy=Start-AzureStorageBlobCopy -AbsoluteUri $ComponentsAccessSAS -DestContainer $containerName -DestContext $destContext -DestBlob $componentsDestBlob
+ 
+#Start copy psmp
+$psmpblobcopy=Start-AzureStorageBlobCopy -AbsoluteUri $PsmpAccessSAS -DestContainer $containerName -DestContext $destContext -DestBlob $psmpDestBlob
+ 
+ 
+#Wait for vhd to be fully copied (~40 minutes)
+Get-AzureStorageBlobCopyState -Blob $componentsDestBlob -Container $containerName -Context $destContext -WaitForComplete
+Get-AzureStorageBlobCopyState -Blob $psmpDestBlob -Container $containerName -Context $destContext -WaitForComplete
+ 
+ 
+$componentsblobUri = ($destContext.BlobEndPoint + $containerName + "/" + $componentsDestBlob)
+$psmpblobUri = ($destContext.BlobEndPoint + $containerName + "/" + $psmpDestBlob)
+ 
+ 
+#Create Components Image from blob
+$vmOSType = "Windows"
+$imageName = "PAS-Components-v10.3"
+$imageConfig = New-AzureRmImageConfig -Location $location
+$imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType $vmOSType -OsState Generalized -BlobUri $componentsblobUri
+$image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $resourceGroupName -Image $imageConfig
+ 
+#Create Psmp Image from blob
+$vmOSType = "Linux"
+$imageName = "PAS-Psmp-v10.3"
+$imageConfig = New-AzureRmImageConfig -Location $location
+$imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType $vmOSType -OsState Generalized -BlobUri $psmpblobUri
+$image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $resourceGroupName -Image $imageConfig
