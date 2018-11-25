aad4e93da204d7fedc61d3c0168fb7d7cfd0c5b6
Ziv Lifshits <ziv.lifshits@gmail.com>
2018-11-25 14:40:57 +0200

    Release v10.5 azure (#57)
    * - Add customer network template
    
    * - Update import-pas-images script with multiple components
    
    * - Update azure README file
    
    * CR fixes
    
    - Fixed "agreed"
    - Fixed "comma"

--- azure/import-pas-images.ps1
+++ azure/import-pas-images.ps1
@@ -3,10 +3,18 @@ PARAM(
     [Parameter(Mandatory = $true)]
     [String]
     $location,
-    # Supplied by CyberArk components AccessSAS
+    # Supplied by CyberArk CPM AccessSAS
     [Parameter(Mandatory = $true)]
     [String]
-    $ComponentsAccessSAS,
+    $CpmAccessSAS,
+    # Supplied by CyberArk PVWA AccessSAS
+    [Parameter(Mandatory = $true)]
+    [String]
+    $PvwaAccessSAS,
+    # Supplied by CyberArk PSM AccessSAS
+    [Parameter(Mandatory = $true)]
+    [String]
+    $PsmAccessSAS,
     # Supplied by CyberArk PSMP AccessSAS
     [Parameter(Mandatory = $true)]
     [String]
@@ -14,48 +22,92 @@ PARAM(
 )
  
 #Set variables
+$release = "v10.5"
 $storageName = "cyberarkimages"
 $containerName = "cyberarkimages"
-$componentsDestBlob = "pas-components.vhd"
-$psmpDestBlob = "pas-psmp.vhd"
+$cpmDestBlob = "pas-cpm-$release.vhd"
+$pvwaDestBlob = "pas-pvwa-$release.vhd"
+$psmDestBlob = "pas-psm-$release.vhd"
+$psmpDestBlob = "pas-psmp-$release.vhd"
 $resourceGroupName = "Cyberark-Images"
- 
-#Create Resource Group
-New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
- 
-#Create Storage Account
-$storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageName -Location $location -SkuName Standard_LRS
-$destContext = $storageAccount.Context
- 
-#Create Blob Storage Container
-New-AzureStorageContainer -Name $containerName -Permission Off -Context $destContext
- 
-#Start copy components
-Start-AzureStorageBlobCopy -AbsoluteUri $ComponentsAccessSAS -DestContainer $containerName -DestContext $destContext -DestBlob $componentsDestBlob
- 
-#Start copy psmp
-Start-AzureStorageBlobCopy -AbsoluteUri $PsmpAccessSAS -DestContainer $containerName -DestContext $destContext -DestBlob $psmpDestBlob
- 
- 
-#Wait for vhd to be fully copied (~40 minutes)
-Get-AzureStorageBlobCopyState -Blob $componentsDestBlob -Container $containerName -Context $destContext -WaitForComplete
-Get-AzureStorageBlobCopyState -Blob $psmpDestBlob -Container $containerName -Context $destContext -WaitForComplete
- 
- 
-$componentsblobUri = ($destContext.BlobEndPoint + $containerName + "/" + $componentsDestBlob)
-$psmpblobUri = ($destContext.BlobEndPoint + $containerName + "/" + $psmpDestBlob)
- 
- 
-#Create Components Image from blob
-$vmOSType = "Windows"
-$imageName = "PAS-Components-v10.3"
-$imageConfig = New-AzureRmImageConfig -Location $location
-$imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType $vmOSType -OsState Generalized -BlobUri $componentsblobUri
-$image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $resourceGroupName -Image $imageConfig
- 
-#Create Psmp Image from blob
-$vmOSType = "Linux"
-$imageName = "PAS-Psmp-v10.3"
-$imageConfig = New-AzureRmImageConfig -Location $location
-$imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType $vmOSType -OsState Generalized -BlobUri $psmpblobUri
-$image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $resourceGroupName -Image $imageConfig
+
+Try
+{
+    #Create Resource Group
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location -Force
+     
+    #Create Storage Account, if not exists
+    $storageAccount = Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageName
+    if (!$storageAccount)
+    {  
+        $storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageName -Location $location -SkuName Standard_LRS 
+    }
+    $destContext = $storageAccount.Context
+
+    #Create Blob Storage Container, if not exists
+    if (!(Get-AzureStorageContainer -Name $containerName -Context $destContext))
+    {
+        New-AzureStorageContainer -Name $containerName -Permission Off -Context $destContext
+    }
+     
+    #Start copy cpm
+    Start-AzureStorageBlobCopy -AbsoluteUri $CpmAccessSAS -DestContainer $containerName -DestContext $destContext -DestBlob $cpmDestBlob
+    
+    #Start copy pvwa
+    Start-AzureStorageBlobCopy -AbsoluteUri $PvwaAccessSAS -DestContainer $containerName -DestContext $destContext -DestBlob $pvwaDestBlob
+     
+    #Start copy psm
+    Start-AzureStorageBlobCopy -AbsoluteUri $PsmAccessSAS -DestContainer $containerName -DestContext $destContext -DestBlob $psmDestBlob
+      
+    #Start copy psmp
+    Start-AzureStorageBlobCopy -AbsoluteUri $PsmpAccessSAS -DestContainer $containerName -DestContext $destContext -DestBlob $psmpDestBlob
+     
+     
+    #Wait for vhd to be fully copied (~40 minutes)
+    Get-AzureStorageBlobCopyState -Blob $cpmDestBlob -Container $containerName -Context $destContext -WaitForComplete
+    Get-AzureStorageBlobCopyState -Blob $pvwaDestBlob -Container $containerName -Context $destContext -WaitForComplete
+    Get-AzureStorageBlobCopyState -Blob $psmDestBlob -Container $containerName -Context $destContext -WaitForComplete
+    Get-AzureStorageBlobCopyState -Blob $psmpDestBlob -Container $containerName -Context $destContext -WaitForComplete
+     
+     
+    $cpmblobUri = ($destContext.BlobEndPoint + $containerName + "/" + $cpmDestBlob)
+    $pvwablobUri = ($destContext.BlobEndPoint + $containerName + "/" + $pvwaDestBlob)
+    $psmblobUri = ($destContext.BlobEndPoint + $containerName + "/" + $psmDestBlob)
+    $psmpblobUri = ($destContext.BlobEndPoint + $containerName + "/" + $psmpDestBlob)
+     
+     
+    #Create Cpm Image from blob
+    $vmOSType = "Windows"
+    $imageName = "PAS-CPM-$release"
+    $imageConfig = New-AzureRmImageConfig -Location $location
+    $imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType $vmOSType -OsState Generalized -BlobUri $cpmblobUri
+    $image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $resourceGroupName -Image $imageConfig
+    
+    #Create Pvwa Image from blob
+    $vmOSType = "Windows"
+    $imageName = "PAS-PVWA-$release"
+    $imageConfig = New-AzureRmImageConfig -Location $location
+    $imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType $vmOSType -OsState Generalized -BlobUri $pvwablobUri
+    $image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $resourceGroupName -Image $imageConfig
+    
+    #Create Psm Image from blob
+    $vmOSType = "Windows"
+    $imageName = "PAS-PSM-$release"
+    $imageConfig = New-AzureRmImageConfig -Location $location
+    $imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType $vmOSType -OsState Generalized -BlobUri $psmblobUri
+    $image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $resourceGroupName -Image $imageConfig
+    
+    #Create Psmp Image from blob
+    $vmOSType = "Linux"
+    $imageName = "PAS-PSMP-$release"
+    $imageConfig = New-AzureRmImageConfig -Location $location
+    $imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType $vmOSType -OsState Generalized -BlobUri $psmpblobUri
+    $image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $resourceGroupName -Image $imageConfig
+}
+Catch
+{
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    Write-Host("Error: $ErrorMessage")
+    Break
+}
